55fff6338683d45b203527cf08f94c5959e016a3
ab-morfik <morfikabderrahim@gmail.com>
2024-10-03 12:22:28 +0200

    1st vesrion

--- convert.sh
+++ convert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function extract_hosts {
-work_dir="/var/lib/awx/projects/inv"	
-cat ${work_dir}/idmag.src | while read l
+#work_dir="/var/lib/awx/projects/inv"	
+cat idmag.src | while read l
   do
     	  
     printf "pos$(echo "${l}" | awk -F '|' '{ print $2}' | sed s'/ //'g) \
@@ -13,19 +13,19 @@ done
 }
 echo
 echo "GENERATION FICHIER INI"
-extract_hosts > /var/lib/awx/projects/inv/idmag.ini && \
+extract_hosts > idmag.ini && \
 echo "DONE"
 echo "REAJUSTEMENT DES GROUPES"
 
-sed -i -e s'/ansible_group=1/ansible_group=MARJANE/'g  ${work_dir}/idmag.ini;
-sed -i -e s'/ansible_group=2/ansible_group=MARKET/'g  ${work_dir}/idmag.ini;
-sed -i -e s'/ansible_group=3/ansible_group=DIGIBAY/'g ${work_dir}/idmag.ini;
-sed -i -e s'/ansible_group=4/ansible_group=OTOP/'g  ${work_dir}/idmag.ini;
-sed -i -e s'/ansible_group=5/ansible_group=CITY/'g  ${work_dir}/idmag.ini;
-sed -i -e s'/ansible_group=6/ansible_group=OVS/'g  ${work_dir}/idmag.ini;
-sed -i -e s'/ansible_group=9/ansible_group=TEST/'g  ${work_dir}/idmag.ini;
+sed -i -e s'/ansible_group=1/ansible_group=MARJANE/'g idmag.ini;
+sed -i -e s'/ansible_group=2/ansible_group=MARKET/'g  idmag.ini;
+sed -i -e s'/ansible_group=3/ansible_group=DIGIBAY/'g idmag.ini;
+sed -i -e s'/ansible_group=4/ansible_group=OTOP/'g  idmag.ini;
+sed -i -e s'/ansible_group=5/ansible_group=CITY/'g  idmag.ini;
+sed -i -e s'/ansible_group=6/ansible_group=OVS/'g  idmag.ini;
+sed -i -e s'/ansible_group=9/ansible_group=TEST/'g idmag.ini;
 echo "DONE";
-ansible-inventory -i ${work_dir}/idmag.ini --list -y > ${work_dir}/inventory.yaml;
+ansible-inventory -i idmag.ini --list -y > inventory.yaml;
 
 
 #END
